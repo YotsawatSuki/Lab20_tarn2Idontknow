c684f12d67e303b2b59923fb67739c595242c52a
Yotsawat Sukinee <yotsawat2557@gmail.com>
2023-02-23 21:53:01 +0700

    Finish Lab20_2

--- lab20_2.cpp
+++ lab20_2.cpp
@@ -5,8 +5,12 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect *R1, Rect *R2){
+	double x = min((R1->x+R1->w),(R2->x+R2->w))-max(R1->x,R2->x);
+	double y = min((R1->y+R1->h),(R2->y+R2->h))-(R2->y);
+	if(x>0 && y>0){
+		return x*y;
+	}else{
+		return 0;
+	}
 }
